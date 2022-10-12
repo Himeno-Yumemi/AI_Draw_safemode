e177ae0a5d8553203bcd15601610d8e4a26d1331
姬野梦美 <49085794+jiyemengmei@users.noreply.github.com>
2022-10-13 01:54:39 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # AI_Draw_safemode
-<b>本插件为AI_image_gen的辅助插件，请自行下载AI_image_gen<b>
+<b>本插件为AI_image_gen的扩展插件，目前已内置无冲突AI_image_gen(测试用)<b>
 ## 功能介绍
-通过上传图片参数至数据库，来实现快捷的图片预览、图片TAG获取、对应标签的ai图片生成等功能。
+通过上传图片参数至数据库，来实现快捷的图片预览、图片TAG获取、对应标签的ai图片生成，图片打分，分数排行显示等功能。若不想使用ai鉴黄，后续将更新纯净版。
 ## 具体操作
-1、下载<code>aidraw_db.py</code>和<code>fz.TTF</code>，放到<code>hoshino/modules/aidrawDB</code>文件夹中\
+1、下载<code>aidraw_db.py</code>和<code>check_iamge.py</code>,<code>fz.TTF</code>，放到<code>hoshino/modules/aidrawDB</code>文件夹中\
 2、在<code>hoshino/config/__bot__.py</code>文件中，<code>MODULES_ON</code>里添加 "aidrawDB"\
-3、【重要！！】<b>打开aidraw_db.py，在20行填入ai绘图的API接口，在21行填入个人token</b>\
+3、【重要！！】<b>打开aidraw_db.py，在21行填入ai绘图的API接口，在22行填入个人token</b>\
+4、【重要！！】<b>打开check_image.py，在5行填入百度图像审核API Key，在6行填入百度图像审核Secret Key</b>\
+具体获取流程参考:\
+<a href="https://github.com/pcrbot/SetuScore" target="_BLANK">SetuScore</a>\
 PS:若不喜欢本字体可自行更换，但文件名必须是<code>fz.TTF</code>
 ## 功能指令
 |  指令   | 说明  | 具体  |
@@ -19,7 +22,11 @@ PS:若不喜欢本字体可自行更换，但文件名必须是<code>fz.TTF</cod
 | 修改配方<b>[序号][tags]</b>  | 修改炼金配方内容  | <b>具体看下方使用演示</b>  |
 | 使用配方<b>[序号]</b>  | 使用炼金配方内容  | <b>[序号]</b>为图片标签  |
 | 删除配方<b>[序号]</b>  | 删除炼金配方内容  | <b>[序号]</b>为图片标签  |
-PS:上传与修改仅限管理员使用，删除仅限超级管理员使用
+| 测试[tags]  | 进行ai绘图，返回种子和分数  |
+| 分数排行[序号  | 查看图片分数排行  | [序号]为TOP数 
+| 分数排行总榜  | 查看全部图片分数排行  |
+| 清理分数排行  | 清除除排行前15的所有数据  |
+PS:上传与修改仅限管理员使用，删除仅限超级管理员使用  |
 ## 使用演示
 <b>绘图数据库帮助:</b>\
 ![image](https://github.com/jiyemengmei/AI_Draw_safemode/blob/main/image/%E7%BB%98%E5%9B%BE%E6%95%B0%E6%8D%AE%E5%BA%93%E5%B8%AE%E5%8A%A9.png)\
@@ -41,7 +48,8 @@ PS:上传与修改仅限管理员使用，删除仅限超级管理员使用
 ![image](https://github.com/jiyemengmei/AI_Draw_safemode/blob/main/image/%E5%88%A0%E9%99%A4%E9%85%8D%E6%96%B9.png)
 ## 鸣谢
 <a href="https://github.com/Mrs4s/go-cqhttp" target="_BLANK">go-cqhttp</a>\
-<a href="https://github.com/Ice-Cirno/HoshinoBot" target="_BLANK">HoshinoBot</a>
+<a href="https://github.com/Ice-Cirno/HoshinoBot" target="_BLANK">HoshinoBot</a>\
+还有群友们的DEBUG测试
 ## 友情链接
 <a href="https://github.com/CYDXDianXian/AI_image_gen" target="_BLANK">AI_image_gen</a>
 ## API
@@ -52,4 +60,6 @@ PS:上传与修改仅限管理员使用，删除仅限超级管理员使用
 ### 2022/10/11
 重构代码\
 新增回复消息上传(减轻刷屏)，修改配方，数据库内容总览\
-制作中:TAG筛选生成，图片ai鉴黄
+### 2022/10/12
+修复BUG\
+新增图片分数显示，图片ai鉴黄筛选，分数排行榜
--- aidraw_db.py
+++ aidraw_db.py
@@ -7,6 +7,7 @@ from hoshino.typing import CQEvent
 from aiocqhttp.exceptions import ActionFailed
 from PIL import Image, ImageDraw,ImageFont
 from io import BytesIO
+from .check_iamge import porn_pic_index
 
 import base64
 import json
@@ -17,16 +18,17 @@ import sqlite3
 import os
 
 ####替换成自己的API
-word2img_url = "http://IP:PORT//got_image?tags="   #绘图API接口
-token = "&token=YOUR TOKEN"  #个人token
+word2img_url = "http://YOURIP:PORT//got_image?tags="   #绘图API接口
+token = "&token=YOURTOKEN"  #个人token
 
-quality=100 #炼金大全总表生成图片质量，若图片过大自行降低
-lmt = FreqLimiter(30) #频率限制(s)
+quality=100 #炼金大全总表生成图片质量，代码内置图片质量检测，一般不需要更改
+lmt = FreqLimiter(5) #频率限制(s)
 dlmt_ = 20 # 每日次数限制
 dlmt = DailyNumberLimiter(dlmt_)
 
 sv_help = '''
 【AI绘图数据库】
+-----------------------------------------------------------------
 -【上传【参数】】  上传图片和tag至数据库内，[参数]为ai绘图的指令
 -【'回复'[bot昵称]上传】  同为上传
 -【炼金大全】  查询炼金手册全部内容
@@ -37,6 +39,11 @@ sv_help = '''
 -【使用配方]序号]】  使用炼金配方内容，[序号]为图片标签
 -【删除配方[序号]】  删除炼金配方内容，[序号]为图片标签
 PS:上传仅限管理员使用，删除仅限超级管理员使用
+-----------------------------------------------------------------
+-【测试[tags]】  进行ai绘图，返回种子和分数
+-【分数排行[序号]】  查看图片分数排行，[序号]为TOP数
+-【分数排行总榜】  查看全部图片分数排行
+-【清理分数排行】  清除除排行前15的所有数据
 '''
 
 sv = Service(
@@ -49,15 +56,83 @@ sv = Service(
     help_ = sv_help #帮助文本
     )
 
-@sv.on_fullmatch(["绘图数据库帮助"])
+@sv.on_fullmatch(('帮助', '数据库帮助', '绘图帮助'), only_to_me=True)
 async def bangzhu(bot, ev):
     await bot.send(ev, await wordimage(sv_help), at_sender=True)
 
 lock = Lock()
 curpath = dirname(__file__)
 image_list_db = join(curpath, 'save_tags.db')   #保存tags数据库
+score_path = join(curpath,'scoreDB.db')
+score_image = join(curpath,'ScoreImage')
 save_image_path= join(curpath,'SaveImage')  # 保存图片路径
 font_path = join(curpath,"fz.TTF")  #字体文件路径
+
+class ScoreCounter:
+    def __init__(self):
+        os.makedirs(os.path.dirname(score_path), exist_ok=True)
+        self._score_table()
+        if not exists(score_image):
+            os.mkdir(score_image)
+        
+    def _connect(self):
+        return sqlite3.connect(score_path)
+
+    def _score_table(self):
+        try:
+            self._connect().execute('''CREATE TABLE IF NOT EXISTS aiscore
+                        (分数 TINYINT,
+                        图片 BLOB,
+                        标签 TEXT,
+                        种子 INT,
+                        形状 CHAR);''')
+        except:
+            raise Exception('创建表发生错误')    
+        
+    def _add_score(self,score,image,tags,seed,shape):
+        try:
+            with self._connect() as conn:
+                conn.execute(
+                    "INSERT INTO aiscore VALUES (?,?,?,?,?)",(score,image,tags,seed,shape))
+            return 1
+        except:
+            raise Exception('添加分数发生错误')
+    
+    def _delete_score(self):
+        try:
+            with self._connect() as conn:
+                r = conn.execute("SELECT 图片 FROM aiscore WHERE rowid NOT IN ( SELECT rowid FROM aiscore where rowid ORDER BY 分数 desc LIMIT 15)").fetchall()
+                num=(len(r))
+                for i in range(0,len(r)):
+                    os.remove(r[i][0])
+                conn.execute("DELETE FROM aiscore WHERE rowid NOT IN ( SELECT rowid FROM aiscore where rowid ORDER BY 分数 desc LIMIT 15)")
+            return num
+        except:
+            raise Exception('清除分数发生错误')
+
+    def _get_score_list(self, num):
+        try:
+            with self._connect() as conn:
+                r = conn.execute("SELECT 分数,图片 FROM aiscore WHERE rowid ORDER BY 分数 desc LIMIT ?",(num,)).fetchall()           
+            return r if r else {}
+        except:
+            raise Exception('分数排行榜发生错误')
+
+    def _vacuum_score(self):
+        try:
+            conn = self._connect()
+            conn.execute('vacuum')
+            conn.close
+        except:
+            raise Exception('刷新分数发生错误') 
+
+    def _get_score_rowid(self):
+        try:
+            with self._connect() as conn:
+                r = conn.execute("select rowid from aiscore order by rowid desc limit 1").fetchone()
+            return r[0] if r else 0
+        except:
+            raise Exception('分数总数发生错误')
 
 class ImageCounter:
     def __init__(self):
@@ -238,7 +313,7 @@ class Another_code():
 async def upload_header(bot, ev):
     global lock
     if not priv.check_priv(ev, priv.ADMIN):
-        await bot.finish(ev, '上传配方仅限管理员使用', at_sender=True)
+        await bot.send(ev, '上传配方仅限管理员使用', at_sender=True)
         return
     async with lock:
         try:
@@ -247,13 +322,13 @@ async def upload_header(bot, ev):
                     image=str(i)
                     break
         except:
-            await bot.finish(ev,"未获取到图片",at_sender=True)
+            await bot.send(ev,"未获取到图片",at_sender=True)
             return
         try:
             msg = SaveClass()._save_info(image,ev.message,1)
             await bot.send(ev, msg, at_sender=True)
         except Exception as e:
-            await bot.finish(ev, f"发生错误，原因：{e}", at_sender=True)
+            await bot.send(ev, f"发生错误，原因：{e}", at_sender=True)
             return
 
 #获取机器人昵称
@@ -283,16 +358,16 @@ async def replymessage(bot, ev: CQEvent):
     if not (flag1 and flag2):
         return
     if not priv.check_priv(ev, priv.ADMIN):
-        await bot.finish(ev,f"仅限管理员上传！", at_sender=True)
+        await bot.send(ev,f"仅限管理员上传！", at_sender=True)
         return
     try:
         tmsg = await bot.get_msg(self_id=ev.self_id, message_id=int(tmid))
     except ActionFailed:
-        await bot.finish(ev, '该消息已过期，请重新转发~')
+        await bot.send(ev, '该消息已过期，请重新转发~')
         return
     image_url = re.search(r"\[CQ:image,file=(.*),url=(.*)\]", str(tmsg["message"]))
     if not image_url:
-        await bot.finish(ev, '未找到图片~')
+        await bot.send(ev, '未找到图片~')
         return
     file = image_url.group(1)
     pic_url = image_url.group(2)
@@ -311,7 +386,7 @@ async def replymessage(bot, ev: CQEvent):
         msg = SaveClass()._save_info(pic_url,tmsg["message"],0)
         await bot.send(ev, msg, at_sender=True)
     except Exception as e:
-        await bot.finish(ev, f"发生错误，原因：{e}", at_sender=True)
+        await bot.send(ev, f"发生错误，原因：{e}", at_sender=True)
         return
 
 @sv.on_rex((r'^炼金手册([1-9]\d*)$'))
@@ -320,7 +395,7 @@ async def alchemy_book(bot, ev):
     page=int(match.group(1))
     image_list=ImageCounter()._get_image_list(page)
     if image_list == {}:
-        await bot.finish(ev, f"手册该页没有配方哦", at_sender=True)
+        await bot.send(ev, f"手册该页没有配方哦", at_sender=True)
         return
     try:
         target = Image.new('RGB', (1920,1080),(255,255,255))
@@ -350,18 +425,98 @@ async def alchemy_book(bot, ev):
         resultmes = f"[CQ:image,file={imgmes}]"
         await bot.send(ev,resultmes)
     except Exception as e:
-        await bot.finish(ev, f"发生错误，原因：{e}", at_sender=True)
+        await bot.send(ev, f"发生错误，原因：{e}", at_sender=True)
+
+@sv.on_fullmatch(('清理分数排行'))
+async def clear_score_rank(bot,ev):
+    if not priv.check_priv(ev, priv.ADMIN):
+        await bot.send(ev, '清理分数排行榜仅限管理员使用', at_sender=True)
+        return
+    try:
+        num = ScoreCounter()._delete_score()
+        ScoreCounter()._vacuum_score()
+        await bot.send(ev,f'已清理{num}条数据')
+    except Exception as e:
+        await bot.send(ev, f'报错:{e}', at_sender=True)
+
+@sv.on_fullmatch(('分数排行总榜'))
+async def score_all_rank(bot, ev):
+    rowid=ScoreCounter()._get_score_rowid()
+    image_list=ScoreCounter()._get_score_list(rowid)
+    add_page=1040*((rowid-1)//8+1)
+    try:
+        target = Image.new('RGB', (1900,add_page),(255,255,255))
+        for index in range(0,rowid):
+            image_msg=image_list[index]
+            score = image_msg[0]
+            image_path=image_msg[1]
+            region = Image.open(image_path)
+            region = region.convert("RGB")
+            region = region.resize((int(region.width/2),int(region.height/2)))
+            font = ImageFont.truetype(font_path, 36)  # 设置字体和大小
+            draw = ImageDraw.Draw(target)
+            row=index//4+1  #行
+            column= index%4+1   #列
+            target.paste(region,(80*column+384*(column-1),50+100*(row-1)+384*(row-1)))
+            draw.text((80*column+384*(column-1)+int(region.width/2)-18,80+100*(row-1)+384*(row-1)+region.height),str(score).replace(',',''),font=font,fill = (0, 0, 0))
+        result_buffer = BytesIO()
+        target.save(result_buffer, format='JPEG', quality=quality) #质量影响图片大小
+        imgByteArr = int((len(result_buffer.getvalue()))/1024000)
+        if imgByteArr>=5:
+            target.resize(int(target.width/2),int(target.height/2))
+        imgmes = 'base64://' + base64.b64encode(result_buffer.getvalue()).decode()
+        resultmes = f"[CQ:image,file={imgmes}]"
+        await bot.send(ev,resultmes)
+    except Exception as e:
+        await bot.send(ev, f"发生错误，原因：{e}", at_sender=True)
+
+@sv.on_rex((r'^分数排行([1-9]\d*)$'))
+async def score_rank(bot, ev):
+    match = ev['match']
+    page=int(match.group(1))
+    rowid=ScoreCounter()._get_score_rowid()
+    if rowid < page:
+        await bot.send(ev, f"目前只能查看前{rowid}的分数排名", at_sender=True)
+        return
+    image_list=ScoreCounter()._get_score_list(page)
+    add_page=1040*((page-1)//8+1)
+    try:
+        target = Image.new('RGB', (1900,add_page),(255,255,255))
+        for index in range(0,page):
+            image_msg=image_list[index]
+            score = image_msg[0]
+            image_path=image_msg[1]
+            region = Image.open(image_path)
+            region = region.convert("RGB")
+            region = region.resize((int(region.width/2),int(region.height/2)))
+            font = ImageFont.truetype(font_path, 36)  # 设置字体和大小
+            draw = ImageDraw.Draw(target)
+            row=index//4+1  #行
+            column= index%4+1   #列
+            target.paste(region,(80*column+384*(column-1),50+100*(row-1)+384*(row-1)))
+            draw.text((80*column+384*(column-1)+int(region.width/2)-18,80+100*(row-1)+384*(row-1)+region.height),str(score).replace(',',''),font=font,fill = (0, 0, 0))
+        result_buffer = BytesIO()
+        target.save(result_buffer, format='JPEG', quality=quality) #质量影响图片大小
+        imgByteArr = int((len(result_buffer.getvalue()))/1024000)
+        if imgByteArr>=5:
+            target.resize(int(target.width/2),int(target.height/2))
+        imgmes = 'base64://' + base64.b64encode(result_buffer.getvalue()).decode()
+        resultmes = f"[CQ:image,file={imgmes}]"
+        await bot.send(ev,resultmes)
+    except Exception as e:
+        await bot.send(ev, f"发生错误，原因：{e}", at_sender=True)
+
 
 @sv.on_fullmatch(('炼金大全'))
 async def alchemy_book_all(bot, ev):
     image_list=ImageCounter()._get_all_image_list()
     if image_list == {}:
-        await bot.finish(ev, f"炼金手册里没有配方哦", at_sender=True)
+        await bot.send(ev, f"炼金手册里没有配方哦", at_sender=True)
         return
     image_len=int(ImageCounter()._get_rowid())
-    add_page=1080*(image_len//8+1)
+    add_page=1050*((image_len-1)//8+1)
     try:
-        target = Image.new('RGB', (1920,add_page),(255,255,255))
+        target = Image.new('RGB', (1900,add_page),(255,255,255))
         for index in range(0,image_len):
             image_msg=image_list[index]
             rowid = image_msg[0]
@@ -377,18 +532,21 @@ async def alchemy_book_all(bot, ev):
             draw.text((80*column+384*(column-1)+int(region.width/2)-18,80+100*(row-1)+384*(row-1)+region.height),str(rowid).replace(',',''),font=font,fill = (0, 0, 0))
         result_buffer = BytesIO()
         target.save(result_buffer, format='JPEG', quality=quality) #质量影响图片大小
+        imgByteArr = int((len(result_buffer.getvalue()))/1024000)
+        if imgByteArr>=5:
+            target.resize(int(target.width/2),int(target.height/2))
         imgmes = 'base64://' + base64.b64encode(result_buffer.getvalue()).decode()
         resultmes = f"[CQ:image,file={imgmes}]"
         await bot.send(ev,resultmes)
     except Exception as e:
-        await bot.finish(ev, f"发生错误，原因：{e}", at_sender=True)
+        await bot.send(ev, f"发生错误，原因：{e}", at_sender=True)
 
 @sv.on_rex((r'^查看配方([1-9]\d*)'))
 async def view_recipe(bot, ev):
     uid = str(ev['user_id'])
     cd=Another_code()._Imt_check(uid)
     if cd != 0:
-        await bot.finish(ev,cd,at_sender=True)
+        await bot.send(ev,cd,at_sender=True)
         return
     else:
         lmt.start_cd(uid,10)
@@ -397,10 +555,10 @@ async def view_recipe(bot, ev):
         rowid=int(match.group(1))
         image_msg = ImageCounter()._get_image(rowid)
         if image_msg == 0:
-            await bot.finish(ev, f"手册该页没有配方哦", at_sender=True)
+            await bot.send(ev, f"未找到{rowid}配方哦", at_sender=True)
             return
     except Exception as e:
-        await bot.finish(ev, f"发生错误，原因：{e}", at_sender=True)
+        await bot.send(ev, f"发生错误，原因：{e}", at_sender=True)
         return
     try:
         scale=image_msg[0]
@@ -417,26 +575,34 @@ async def view_recipe(bot, ev):
         msg=f"配方序号为:{rowid}\n{resultmes}\nai绘图{tags}&scale={scale}&shape={shape}"
         await bot.send(ev,msg,at_sender=True)
     except Exception as e:
-        await bot.finish(ev, f"发生错误，原因：{e}", at_sender=True)
+        await bot.send(ev, f"发生错误，原因：{e}", at_sender=True)
 
 @sv.on_rex((r'^使用配方([1-9]\d*)'))
 async def generate_recipe(bot, ev):
     uid = str(ev['user_id'])
     dImt_msg=Another_code()._dlmt_check(uid)
     if dImt_msg != 0:
-        await bot.finish(ev, dImt_msg, at_sender=True)
+        await bot.send(ev, dImt_msg, at_sender=True)
         return
     else:
         dlmt.increase(uid)
     Imt_msg=Another_code()._Imt_check(uid)
     if Imt_msg != 0:
-        await bot.finish(ev, Imt_msg, at_sender=True)
+        await bot.send(ev, Imt_msg, at_sender=True)
         return
     else:
         lmt.start_cd(uid)
     try:
         match = ev['match']
         rowid=int(match.group(1))
+        image_msg = ImageCounter()._get_image(rowid)
+        if image_msg == 0:
+            await bot.send(ev, f"未找到{rowid}配方哦", at_sender=True)
+            return
+    except Exception as e:
+        await bot.send(ev, f"发生错误，原因：{e}", at_sender=True)
+        return
+    try:
         image_list=ImageCounter()._get_image(rowid)
         scale=image_list[0]
         size=image_list[1]
@@ -444,15 +610,25 @@ async def generate_recipe(bot, ev):
         tags=image_list[2]
         msg=f"{tags}&scale={scale}&shape={shape}".replace('&r18=1','')
         await bot.send(ev, f"\n正在炼金中，请稍后...\n(今日剩余{dlmt_-int(dlmt.get_num(uid))}次)", at_sender=True)
-        image = await gen_pic(msg)
-        await bot.send(ev,image,at_sender=True)
+        image,score = await gen_pic(msg)
+        await bot.send(ev,image+f"\n分数:{score}",at_sender=True)
     except Exception as e:
-        await bot.finish(ev, f"发生错误，原因：{e}", at_sender=True)
+        await bot.send(ev, f"发生错误，原因：{e}", at_sender=True)
 
 @sv.on_rex((r'^修改配方([1-9]\d*)'))
 async def update_recipe(bot, ev):
     if not priv.check_priv(ev, priv.ADMIN):
-        await bot.finish(ev, '修改配方仅限管理员使用', at_sender=True)
+        await bot.send(ev, '修改配方仅限管理员使用', at_sender=True)
+        return
+    try:
+        match = ev['match']
+        rowid=int(match.group(1))
+        image_msg = ImageCounter()._get_image(rowid)
+        if image_msg == 0:
+            await bot.send(ev, f"未找到{rowid}配方哦", at_sender=True)
+            return
+    except Exception as e:
+        await bot.send(ev, f"发生错误，原因：{e}", at_sender=True)
         return
     try:
         match = ev['match']
@@ -464,39 +640,54 @@ async def update_recipe(bot, ev):
         msg=ImageCounter()._update_image(tags,rowid)
         await bot.send(ev,msg,at_sender=True)
     except Exception as e:
-        await bot.finish(ev, f"发生错误，原因：{e}", at_sender=True)
+        await bot.send(ev, f"发生错误，原因：{e}", at_sender=True)
 
 @sv.on_rex((r'^删除配方([1-9]\d*)'))
 async def delete_recipe(bot, ev):
     if not priv.check_priv(ev, priv.SUPERUSER):
-        await bot.finish(ev, '删除配方仅限超级管理员使用', at_sender=True)
+        await bot.send(ev, '删除配方仅限超级管理员使用', at_sender=True)
         return
-    #try:
-    match = ev['match']
-    rowid=int(match.group(1))
-    msg=ImageCounter()._delete_image(rowid)
-    ImageCounter()._vacuum_image()
-    await bot.send(ev,msg,at_sender=True)
-    """ except Exception as e:
-        await bot.finish(ev, f"发生错误，原因：{e}", at_sender=True) """
+    try:
+        match = ev['match']
+        rowid=int(match.group(1))
+        image_msg = ImageCounter()._get_image(rowid)
+        if image_msg == 0:
+            await bot.send(ev, f"未找到{rowid}配方哦", at_sender=True)
+            return
+    except Exception as e:
+        await bot.send(ev, f"发生错误，原因：{e}", at_sender=True)
+        return
+    try:
+        match = ev['match']
+        rowid=int(match.group(1))
+        msg=ImageCounter()._delete_image(rowid)
+        ImageCounter()._vacuum_image()
+        await bot.send(ev,msg,at_sender=True)
+    except Exception as e:
+        await bot.send(ev, f"发生错误，原因：{e}", at_sender=True)
 
 async def gen_pic(text):
     try:
-        get_url = word2img_url + text + token
+        get_url = str(word2img_url + text + token).replace('amp;','')
         res = await aiorequests.get(get_url)
         image = await res.content
         load_data = json.loads(re.findall('{"steps".+?}', str(image))[0])
         image_b64 = 'base64://' + str(base64.b64encode(image).decode())
+        try:
+            score = (await porn_pic_index(str(base64.b64encode(image).decode())))["value"]
+        except:
+            score = 0
         mes = f"[CQ:image,file={image_b64}]"
         mes += f'\nseed:{load_data["seed"]}'       
         """ mes += f'\tscale:{load_data["scale"]}\n'
         mes += f'tags:{text}' """
-        return mes
+        return mes,score
     except Exception as e:
-        return f"炼金失败了,原因:{e}"
+        print(e)
+        return f"炼金失败了"
 
 async def wordimage(word):
-    bg = Image.new('RGB', (950,450), color=(255,255,255))
+    bg = Image.new('RGB', (950,550), color=(255,255,255))
     font = ImageFont.truetype(font_path, 30)  # 设置字体和大小
     draw = ImageDraw.Draw(bg)
     draw.text((10,5), word, fill="#000000", font=font)
@@ -505,3 +696,42 @@ async def wordimage(word):
     imgmes = 'base64://' + base64.b64encode(result_buffer.getvalue()).decode()
     resultmes = f"[CQ:image,file={imgmes}]"
     return resultmes
+
+@sv.on_prefix(('测试'))
+async def get_image(bot,ev):
+    url = (word2img_url + str(ev.message) + token).replace('amp;','')
+    res = await aiorequests.get(url)
+    image = await res.content
+    load_data = json.loads(re.findall('{"steps".+?}', str(image))[0])
+    seed=load_data['seed']
+    image_base64=str(base64.b64encode(image).decode())
+    image_b64 = 'base64://' + image_base64
+    try:
+        score = (await porn_pic_index(image_base64))["value"]
+    except:
+        score = 0
+    resultmes = f"[CQ:image,file={image_b64}]"
+    if score <460 and score !=0:
+        try:
+            datetime = calendar.timegm(time.gmtime())
+            img = Image.open(BytesIO(image)).convert("RGB")
+            saveconfig = join(score_image, f'{str(datetime)}.png')
+            size=f"{img.width}x{img.height}"
+            pic = open(saveconfig, "wb")
+            pic.write(image)
+            pic.close()
+        except Exception as e:
+            await bot.send(ev,f"报错:{e}",at_sender=True)
+            return
+        try:
+            shape = Another_code()._size_to_shape(size)
+            tags=(str(ev.message).split('&'))[0]
+            ScoreCounter()._add_score(score,saveconfig,tags,seed,shape)
+            await bot.send(ev,resultmes+f"\nseed:{seed}\t分数:{score}",at_sender=True)
+        except Exception as e:
+            await bot.send(ev,f"报错:{e}",at_sender=True)
+            return
+    elif score ==0:
+        await bot.send(ev,"图片质量堪忧,不予显示",at_sender=True)
+    else:
+        await bot.send(ev,"图片太涩了,不予显示",at_sender=True)
